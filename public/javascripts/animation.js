8530a4208fabd6cccf0035d120ecdc73ab39e747
Adebayo Ayomide <adebayormide@gmail.com>
2023-10-05 02:57:10 -0400

    Homepage completed

--- public/javascripts/animation.js
+++ public/javascripts/animation.js
@@ -1,21 +1,2 @@
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    const elements = document.querySelectorAll('.fade-in-up');
-
-    window.addEventListener('scroll', function () {
-        elements.forEach(function (element) {
-            if (isInViewport(element)) {
-                element.classList.add('animated');
-            }
-        });
-    });
-});
+// Load Font Awesome icons
+document.write('<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"><\/script>');
